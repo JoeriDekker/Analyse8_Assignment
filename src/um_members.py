822410f28a116152c14a2b62da9e787e1dab8181
JoeriDekker <joeridekker2002@gmail.com>
2024-05-14 14:31:52 +0200

    very good epic start

---
+++ src/um_members.py
@@ -0,0 +1,15 @@
+# ==========[CREATED BY]============
+# STUDENT 1
+# NAME: Joeri Dekker
+# STUDENT NUMBER:
+
+# STUDENT 2
+# NAME: Alysha van Etten
+# STUDENT NUMBER: 
+
+# STUDENT 3
+# NAME: Kim leeters
+# STUDENT NUMBER:
+# ===================================
+
+print("This is a very epic python project biatches")
